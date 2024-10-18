6919b4bd8d3a96137bb1bb2b67b1cb419a5ee741
Eduardo Lima <edu.fen@outlook.com.br>
2024-10-18 17:20:18 +0000

    day3

--- models/intermediate/int_address_prep.sql
+++ models/intermediate/int_address_prep.sql
@@ -16,11 +16,14 @@ with
     address as (
         select
         address.address_id
+        , stateprovince.stateprovince_code
         , address.stateprovince_id
+        , stateprovince.state_name
+        , stateprovince.countryregion_code
+        , country.country_name
         , address.address_line
         , address.postal_code
         , address.city as city_name
-        , country.country_name
         from {{ ref('stg_erp__address') }} as address
         left join stateprovince on address.stateprovince_id = stateprovince.stateprovince_id
         left join country on stateprovince.countryregion_code = country.countryregion_code
