60b8207bd5c564540b357136dbbcab3666b81aa0
Eduardo Lima <edu.fen@outlook.com.br>
2024-10-18 19:35:01 +0000

    atualizando customer2

--- models/intermediate/int_customer_prep.sql
+++ models/intermediate/int_customer_prep.sql
@@ -15,28 +15,21 @@ with
         from {{ ref('stg_erp__person') }}
     )
 
-    , businessentity as (
-        select
-        businessentity_id
-        , person_id
-
-        from {{ ref('stg_erp__businessidentitycontact') }}
-    )
-
     , joined as (
         select
-        customer.customer_id
+        row_number () over (order by customer_id) as customer_sk
+        , customer.customer_id
         , customer.person_id
         , customer.territory_id
         , person.businessentity_id
         , person.name
         , person.person_type
         from customer
-        left join businessentity on customer.person_id = businessentity.person_id
-        left join person on businessentity.businessentity_id = person.businessentity_id
+        left join person on customer.person_id = person.businessentity_id
     )
 
 
-    select * from joined
-    where name is not null
-    limit 50;
+select * from joined
+where name is not NULL
+
+
